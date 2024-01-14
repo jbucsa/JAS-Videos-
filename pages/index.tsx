3f121480f3f84f11921181e40910e422f3f40719
jbucsa <justin.bucsa@gmail.com>
2024-01-14 10:08:40 -0600

    Cleaned up Auth background.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return (
+        <div><div>Hello, Next.js!</div><div>
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
+        </div></div>
+    )
   }
