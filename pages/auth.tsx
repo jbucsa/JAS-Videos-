1d727da0402c97d0be823b7e592055ea89cefc2e
jbucsa <justin.bucsa@gmail.com>
2024-01-14 10:20:06 -0600

    New Components Folder.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,19 +2,17 @@ import '../styles/globals.css';
 
 const Auth = () => {
     return (
-        <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
                 <div>
-                    <nav className="px-12 py-10 h-1/10">
-                        <img src="/images/logo.png" alt="Logo Here" className="h-1/10 w-1/12"> 
+                    <nav className="px-12 py-10">
+                        <img src="/images/logo.png" alt="Logo Here" className="h-12"> 
                         </img>
                     </nav>
                 </div> 
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 
-                    lg:w-2/5 lg:max-w-md
-                    // Large screens 
-                    rounded-md w-full">
+                    lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
                             Sign In
                         </h2>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 export default function Page() {
     return (
-        <div><div>Hello, Next.js!</div><div>
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
-        </div></div>
+        <div>
+            <div>Hello, Next.js!
+            </div>
+            <div>
+                <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
+            </div>
+        </div>
     )
   }
