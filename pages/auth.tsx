2e343e30f01e0bd3f5ee129895a36c97d1f08295
sjcowdery96 <sjc.cowdery@gmail.com>
2024-01-14 18:10:10 -0600

    corrected auth loading time

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,13 +1,19 @@
-const Auth = () => {
+//create our sign-in/authorizion page
+//lesson learned -- anonymous functions take FOREVER to load
+
+function Auth() {
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-            
             <div className="bg-black w-full h-full lg:bg-opacity-50">
                 <nav className="px-12 py-5">
-                    <img src="/images/logo.png" alt="Logo Here" className="h-12"/>
-                </nav> 
+                    <img src="/images/logo.png" alt="Logo Here" className="h-12" />
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 py-16 px-16 self-center mt-2 lg:2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
+                    </div>
+                </div>
             </div>
-            
         </div>
     )
 };
