312487c2410b0951e8eb66696d948c2c306e43a7
jbucsa <justin.bucsa@gmail.com>
2024-01-15 19:41:25 -0600

    slight styling changes to the code in PlayButton.tsx

--- pages/components/PlayButton.tsx
+++ pages/components/PlayButton.tsx
@@ -13,20 +13,20 @@ const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
         <button
             onClick={() => router.push(`/watch/${movieId}`)}
             className="
-        bg-white 
-        rounded-md 
-        py-1 md:py-2 
-        px-2 md:px-4
-        w-auto 
-        text-xs lg:text-lg 
-        font-semibold
-        flex
-        flex-row
-        items-center
-        hover:bg-neutral-300
-        transition
-        "
-        >
+                bg-white 
+                rounded-md 
+                py-1 md:py-2 
+                px-2 md:px-4
+                w-auto 
+                text-xs lg:text-lg 
+                font-semibold
+                flex
+                flex-row
+                items-center
+                hover:bg-neutral-300
+                transition
+                ">
+                    
             <PlayIcon className="w-4 md:w-7 text-black mr-1" />
             Play
         </button>
