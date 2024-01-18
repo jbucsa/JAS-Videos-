fecedd320c32dd3a1171755b07f67e01318689e5
jbucsa <justin.bucsa@gmail.com>
2024-01-18 15:58:40 -0600

    Moved signout testing to its own page, signOut.tsx.

--- pages/home.tsx
+++ pages/home.tsx
@@ -18,41 +18,17 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 //UNFINISHED CONNECTING DATABASE
 //const some_movies = await db.find().limit(10)
 
-// This export is needed to execute a signout from the database
-export async function getServerSideProps( context: NextPageContext){
- const session = await getSession(context); 
 
-if (!session) {
-  return {
-    redirect: {
-      // destination: '/auth' if you followed the tutorials properly 
-      destination: '/index',
-      permanent: false 
-    }
-  }
-}
-
-return {
-  props: {}
-}
-
-
-}
 
 console.log(randomMovies)
 
 
 export default function Home() {
-  // Use the following function to import CurrentUser data
-  const { data: user } = useCurrentUser();
+
   
   return (
     <div>
-      {/* This is a template for signout button */}
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
 
-      {/* This will display current user information */}
-      <p className="text-white"> Logged in as : {user?.name} - {user?.email} </p>
 
       <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
         <div className="bg-black w-full h-full lg:bg-opacity-50">
